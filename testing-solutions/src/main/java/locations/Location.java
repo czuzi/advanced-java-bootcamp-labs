2d2ac77ddeefb9218130ba1212690f925e1b837a
Istvan <palocsayi@gmail.com>
2023-02-10 06:04:41 +0100

    assert distance

--- testing-solutions/src/main/java/locations/Location.java
+++ testing-solutions/src/main/java/locations/Location.java
@@ -43,4 +43,14 @@ public class Location {
 	public boolean isOnPrimeMeridian() {
 		return longitude == 0;
 	}
+
+	public double distanceFrom(Location otherLocation) {
+		int earthRadius = 6371;
+		double latitudeDifference = Math.toRadians(otherLocation.getLatitude() - this.latitude);
+		double longitudeDifference = Math.toRadians(otherLocation.getLongitude() - this.longitude);
+		double a = Math.pow(Math.sin(latitudeDifference / 2), 2) + Math.cos(Math.toRadians(this.latitude))
+				* Math.cos(Math.toRadians(otherLocation.getLatitude())) * Math.pow(Math.sin(longitudeDifference / 2), 2);
+		double b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		return Math.round(earthRadius * b * 10.0) / 10.0;
+	}
 }
--- testing-solutions/src/test/java/locations/LocationTest.java
+++ testing-solutions/src/test/java/locations/LocationTest.java
@@ -17,11 +17,34 @@ class LocationTest {
 
 	@Test
 	void isOnEquator() {
-		assertEquals(true, new Location("Equator", 0, 34.213).isOnEquator());
+		assertTrue(new Location("Equator", 0, 34.213).isOnEquator());
 	}
 
 	@Test
 	void isOnPrimeMeridian() {
-		assertEquals(true, new Location("Meridian", 123.1211, 0).isOnPrimeMeridian());
+		assertTrue(new Location("Meridian", 123.1211, 0).isOnPrimeMeridian());
+	}
+
+	@Test
+	void testDifferentLocationParsers() {
+		LocationParser locationParser = new LocationParser();
+
+		assertNotEquals(locationParser, this.locationParser);
+		assertNotSame(locationParser, this.locationParser);
+	}
+
+	@Test
+	void testBudapestDebrecenDistance() {
+		Location budapest = locationParser.parse("Budapest,47.497912,19.040235");
+		Location debrecen = locationParser.parse("Debrecen,47.52997,21.63916");
+		assertEquals(195.2, budapest.distanceFrom(debrecen));
+
+	}
+
+	@Test
+	void testDistanceWithSameCoordinates() {
+		Location budapest = locationParser.parse("Budapest,47.497912,19.040235");
+		Location budapest2 = locationParser.parse("Budapest,47.497912,19.040235");
+		assertEquals(0.0, budapest.distanceFrom(budapest2));
 	}
 }
