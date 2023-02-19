1e4c95fb61e3ba903374fadc7ae08817b47d8d5c
Istvan <palocsayi@gmail.com>
2023-02-19 22:06:56 +0100

    jpa list, find, remove

--- jpa-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
+++ jpa-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
@@ -4,10 +4,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ActivityTrackerMain {
 
     public static void main(String[] args) {
+
+        ActivityTrackerMain main = new ActivityTrackerMain();
+
         Activity firstActivity = new Activity(LocalDateTime.of(2022, 4, 11, 6, 0), "morning running", ActivityType.RUNNING);
         Activity secondActivity = new Activity(LocalDateTime.of(2022, 4, 9, 10, 15), "hiking near Budapest", ActivityType.HIKING);
         Activity thirdActivity = new Activity(LocalDateTime.of(2022, 3, 14, 17, 30), "evening running", ActivityType.RUNNING);
@@ -16,17 +20,37 @@ public class ActivityTrackerMain {
         EntityManager em = factory.createEntityManager();
 
         try {
-            saveActivity(firstActivity, em);
-            saveActivity(secondActivity, em);
-            saveActivity(thirdActivity, em);
+            main.saveActivity(firstActivity, em);
+            main.saveActivity(secondActivity, em);
+            main.saveActivity(thirdActivity, em);
+
+            List<Activity> activities = main.listActivites(em);
+            System.out.println(activities);
+
+            Activity activity = main.findActivityById(1L, em);
+            System.out.println(activity);
+
+            em.getTransaction().begin();
+            em.remove(activity);
+            em.getTransaction().commit();
+
+            System.out.println(main.listActivites(em));
         } finally {
             em.close();
         }
     }
 
-    private static void saveActivity(Activity firstActivity, EntityManager em) {
+    private void saveActivity(Activity firstActivity, EntityManager em) {
         em.getTransaction().begin();
         em.persist(firstActivity);
         em.getTransaction().commit();
+    }
+
+    private List<Activity> listActivites(EntityManager em) {
+        return em.createQuery("select a from Activity a", Activity.class).getResultList();
+    }
+
+    private Activity findActivityById(Long id, EntityManager em) {
+        return em.find(Activity.class, id);
     }
 }
