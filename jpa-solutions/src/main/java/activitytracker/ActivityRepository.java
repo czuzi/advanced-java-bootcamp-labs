140deaab147c3af3223b5a1e4e3dfcd9d3797601
Istvan <palocsayi@gmail.com>
2023-02-19 22:29:59 +0100

    jpa integration test

---
+++ jpa-solutions/src/main/java/activitytracker/ActivityRepository.java
@@ -0,0 +1,55 @@
+package activitytracker;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class ActivityRepository {
+
+    private EntityManagerFactory factory;
+
+    public ActivityRepository(EntityManagerFactory factory) {
+        this.factory = factory;
+    }
+
+    public void saveActivity(Activity activity) {
+        EntityManager em = factory.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(activity);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Activity> listActivities() {
+        EntityManager em = factory.createEntityManager();
+        try {
+            return em.createQuery("select a from Activity a", Activity.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Activity findActivityById(long id) {
+        EntityManager em = factory.createEntityManager();
+        try {
+            return em.find(Activity.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void deleteActivity(long id) {
+        EntityManager em = factory.createEntityManager();
+        try {
+            Activity activityToDelete = findActivityById(id);
+            em.getTransaction().begin();
+            em.remove(activityToDelete);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+}
---
+++ jpa-solutions/src/test/java/activitytracker/ActivityRepositoryTest.java
@@ -0,0 +1,36 @@
+package activitytracker;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityRepositoryTest {
+
+    EntityManagerFactory factory;
+    ActivityRepository repository;
+
+    @BeforeEach
+    void init() {
+        factory = Persistence.createEntityManagerFactory("test-pu");
+        repository = new ActivityRepository(factory);
+    }
+
+    @Test
+    void testSaveActivity() {
+        Activity activity = new Activity(LocalDateTime.of(2022, 4, 11, 6, 0), "morning running", ActivityType.RUNNING);
+        repository.saveActivity(activity);
+
+        Long id = activity.getId();
+
+        Activity result = repository.findActivityById(id);
+
+        assertEquals("morning running", result.getDescription());
+        assertEquals(ActivityType.RUNNING, result.getType());
+    }
+}
