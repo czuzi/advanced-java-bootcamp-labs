31bb31a03ffc34cde63fd10927bfe1140be6a0f0
Istvan <palocsayi@gmail.com>
2023-04-12 23:55:18 +0200

    mariadb

---
+++ spring-solutions/locations/src/main/java/training360/locations/controllers/repositories/LocationRepository.java
@@ -0,0 +1,6 @@
+package training360.locations.controllers.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository {
+}
